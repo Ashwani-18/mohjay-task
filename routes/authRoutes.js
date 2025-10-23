868e707abee5906111e0ba0e428a8fdd8295fefa
Ashwani-18 <ashwanirajput18000@gmail.com>
2025-10-23 15:43:16 +0530

    superadmin, admin and user based authentication and authorization

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { sendPhoneOtp, verifyPhoneOtp, login } = require("../controllers/authControllers");
+const { sendPhoneOtp, verifyPhoneOtp, login, sendEmailOtp, verifyEmailOtp } = require("../controllers/authControllers");
+const { protect, authorizeRoles } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 
 // for email
-// router.post("/email-send-otp",)
-// router.post("/email-verify-otp",)
+router.post("/email-send-otp",sendEmailOtp)
+router.post("/email-verify-otp",verifyEmailOtp)
 
 // for phone 
 router.post("/phone-send-otp",sendPhoneOtp)
@@ -13,6 +14,21 @@ router.post("/phone-verify-otp",verifyPhoneOtp)
 
 // login 
 router.post("/login",login)
+
+//user
+router.get("/user", protect, authorizeRoles("user", "admin", "superadmin"), (req, res) => {
+  res.json({ message: `Welcome ${req.user.name}, you are logged in as ${req.user.role}` });
+});
+
+// Admin 
+router.get("/admin", protect, authorizeRoles("admin", "superadmin"), (req, res) => {
+  res.json({ message: `Hello ${req.user.name}, this is the Admin route.` });
+});
+
+// SuperAdmin 
+router.get("/superadmin", protect, authorizeRoles("superadmin"), (req, res) => {
+  res.json({ message: `Hello ${req.user.name}, this is the SuperAdmin route.` });
+});
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,20 +1,19 @@
+require("dotenv").config(); 
 const express = require("express");
-const authRoutes = require("./routes/authRoutes")
+const authRoutes = require("./routes/authRoutes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const connectDB = require("./config/db");
 
-
 connectDB();
 app.use(express.json());
 
-app.get("/", (req,res) => {
-    res.send("Hello world")
+app.get("/", (req, res) => {
+    res.send("Hello world");
 });
 
-app.use("/api/v1/auth",authRoutes)
-
+app.use("/api/v1/auth", authRoutes);
 
 app.listen(PORT, () => {
-    console.log("Server is running on port 3000");
-})
+    console.log(`Server is running on port ${PORT}`);
+});
